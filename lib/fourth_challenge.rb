4fa778eab8a11036569b1acfafe52d8afddf1e13
James Batsell <jamesbatsell05@gmail.com>
2019-08-28 07:12:13 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,12 +5,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        name: "Benovolio", 
+        {name: "Benovolio", 
         age: "17"
-        attitude: "worried"
-        name: "Mercutio"
+        attitude: "worried"}
+        {name: "Mercutio"
         age: "18"
-        attitude: "hot-headed"
+        attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -18,9 +18,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        name: "Steven"
+        {name: "Steven"
         age: "30"
-        attitude: "confused"
+        attitude: "confused"}
+        {name: "Nurse"
+        age: "44"
+        attitude: "worried"}
       ]
    }
 }
